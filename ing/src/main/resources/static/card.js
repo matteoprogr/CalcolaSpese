e4b402651a2548d6c50060a8a602f95216923bc8
matteoprogr <matteo93parisi@gmail.com>
2025-08-25 23:31:44 +0200

    eliminati i form negli overlay, refactor css

--- ing/src/main/resources/static/card.js
+++ ing/src/main/resources/static/card.js
@@ -92,60 +92,62 @@ export function categoriaComponent(categoria) {
 
 export async function overlayAddSpesa() {
 const openBtn = document.getElementById('addSpesaBtn');
-const closeBtn = document.getElementById('closeFormBtn');
 const overlay = document.getElementById('spesaFormOverlay');
 const form = document.getElementById('spesaForm');
-const mainContent = document.querySelector('.main-content');
 const categoriaInput = document.getElementById('categoria');
-const datalist = document.getElementById('categorie');
+const saveBtn = document.getElementById("saveBtn");
+const catRow = document.getElementById('categorieCardsAdd');
+const closeBtn = document.getElementById('closeFormBtn');
+const importo = document.getElementById('importo');
+const descrizione = document.getElementById('descrizione');
+const data = document.getElementById('data');
 
-openBtn.addEventListener('click', async () => {
-  overlay.style.display = 'flex';
-  mainContent.classList.add('blur-active');
-  await popolaCategorie(datalist);
-  setTimeout(() => overlay.classList.add('show'), 10);
-});
+openBtn.addEventListener('click', async (e) => {
+        if (overlay.classList.contains('showOverlay')) {
+            overlay.classList.remove('showOverlay');
+        } else {
+            catRow.innerHTML = "";
+            overlay.classList.toggle("showOverlay");
+            const categorie = await getCategorie();
+            categorie.forEach(cat => {
+            const card = catOverlay(cat.categoria, "addSpesa");
+            catRow.appendChild(card);
+            data.value = new Date().toISOString().split("T")[0];
+            });
+        }
+         document.addEventListener('click', (event) => {
+            if (!overlay.classList.contains('showOverlay')) return;
+            if (event.target.closest('#spesaFormOverlay') || event.target.closest('#addSpesaBtn')) return;
+              overlay.classList.remove('showOverlay');
+          });
+    });
+    closeBtn.addEventListener('click',async () =>{
+        overlay.classList.remove('showOverlay');
+    });
 
-closeBtn.addEventListener('click', () => {
-  overlay.classList.remove('show');
-  mainContent.classList.remove('blur-active');
-  setTimeout(() => overlay.style.display = 'none', 300);
-});
+    saveBtn.addEventListener('click', async (e) => {
+      e.preventDefault();
+      const spesa = {
+        categoria: categoriaInput.value.trim(),
+        dataSpesa: document.getElementById('data').value,
+        importo: -Math.abs(parseFloat(importo.value)),
+        descrizione: descrizione.value
+      };
 
-form.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  //const categorie = await getCategorie();
-  const spesa = {
-    categoria: categoriaInput.value.trim(),
-    dataSpesa: document.getElementById('data').value,
-    importo: -Math.abs(parseFloat(document.getElementById('importo').value)),
-    descrizione: document.getElementById('descrizione').value
-  };
-
-  try {
-    await saveSpesa(spesa);
-    overlay.classList.remove('show');
-    mainContent.classList.remove('blur-active');
-    setTimeout(() => overlay.style.display = 'none', 300);
-    createCriteri();
-    form.reset();
-  } catch (err) {
-    showErrorToast("Errore durante il salvataggio:", "error");
-  }
-});
+      try {
+        await saveSpesa(spesa);
+        overlay.classList.remove('showOverlay');
+        createCriteri();
+        categoriaInput.value = "";
+        importo.value = "";
+        descrizione.value = "";
+      } catch (err) {
+        showErrorToast("Errore durante il salvataggio:", "error");
+      }
+    });
 }
 
-  async function popolaCategorie(datalist) {
-    const categorie = await getCategorie(); // supponendo sia un array di stringhe
-    datalist.innerHTML = ""; // svuota le opzioni precedenti
-    categorie.forEach(cat => {
-      const option = document.createElement('option');
-      option.value = cat.categoria;
-      datalist.appendChild(option);
-    });
-  }
-
-  export async function overlayRicerca() {
+export async function overlayRicerca() {
 
     const openBtn = document.getElementById('getSpesaBtn');
     const overlay = document.getElementById('overlayRicerca');
@@ -159,7 +161,7 @@ form.addEventListener('submit', async (e) => {
             overlay.classList.toggle("showOverlay");
             const categorie = await getCategorie();
             categorie.forEach(cat => {
-            const card = catOverlay(cat.categoria);
+            const card = catOverlay(cat.categoria, "getSpese");
             catRow.appendChild(card);
             });
         }
@@ -171,7 +173,7 @@ form.addEventListener('submit', async (e) => {
     });
   }
 
-function catOverlay(categoria) {
+function catOverlay(categoria, sezione) {
         const container = document.createElement("div");
         container.classList.add("card");
            container.innerHTML = `
@@ -181,8 +183,20 @@ function catOverlay(categoria) {
            `;
 
             container.addEventListener("click", () => {
+              const categoriaElement = container.querySelector("span");
               container.classList.toggle("selected");
-              createCriteri();
+              if(sezione === "getSpese"){
+                    createCriteri();
+              }else if(sezione === "addSpesa"){
+                    const categoria = document.getElementById('categoria');
+                    container.classList.toggle("selected");
+                    categoria.value = categoriaElement.innerText;
+              }else if(sezione === "editSpesa"){
+                    const categoria = document.getElementById('editCategoria');
+                    container.classList.toggle("selected");
+                    categoria.value = categoriaElement.innerText;
+              }
+
             });
 
         return container;
@@ -190,37 +204,45 @@ function catOverlay(categoria) {
 
 export async function overlayEdit(spesa) {
     const overlay = document.getElementById('editSpesaFormOverlay');
+    const catRow = document.getElementById('categorieCardsEdit');
+    const editBtn = document.getElementById('editBtn');
     const closeBtn = document.getElementById('closeEditFormBtn');
-    const form = document.getElementById('editSpesaForm');
-    const mainContent = document.querySelector('.main-content');
-    const datalist = document.getElementById('editCategorie');
     const dataInserimento = spesa.dataInserimento;
 
     // Popola i campi del form con i dati della spesa
     document.getElementById('editSpesaId').value = spesa.id;
-    document.getElementById('editCategoria').value = spesa.categoria.trim();
+    const categoriaElement = document.getElementById('editCategoria');
+    categoriaElement.value = spesa.categoria.trim();
     document.getElementById('editData').value = spesa.dataSpesa;
     document.getElementById('editImporto').value = Math.abs(spesa.importo); // Rimuovi il segno negativo per visualizzazione
     document.getElementById('editDescrizione').value = spesa.descrizione;
 
-    // Mostra l'overlay
-    overlay.style.display = 'flex';
-    mainContent.classList.add('blur-active');
-    await popolaCategorie(datalist);
-    setTimeout(() => overlay.classList.add('show'), 10);
 
-    closeBtn.addEventListener('click', () => {
-      overlay.classList.remove('show');
-      mainContent.classList.remove('blur-active');
-      setTimeout(() => overlay.style.display = 'none', 300);
+    closeBtn.addEventListener('click',async () =>{
+        overlay.classList.remove('showOverlay');
     });
 
-    form.addEventListener('submit', async (e) => {
+
+    catRow.innerHTML = "";
+    overlay.classList.add("showOverlay");
+    const categorie = await getCategorie();
+    categorie.forEach(cat => {
+    const card = catOverlay(cat.categoria, "editSpesa");
+    catRow.appendChild(card);
+    });
+    document.addEventListener('click', (event) => {
+        if (!overlay.classList.contains('showOverlay')) return;
+        if (event.target.closest('#editSpesaFormOverlay')) return;
+          overlay.classList.remove('showOverlay');
+      });
+
+
+    editBtn.addEventListener('click', async (e) => {
     e.preventDefault();
 
     const spesa = {
         id: parseInt(document.getElementById('editSpesaId').value),
-        categoria: document.getElementById('editCategoria').value.trim(),
+        categoria: categoriaElement.value,
         dataInserimento: dataInserimento,
         dataSpesa: document.getElementById('editData').value,
         importo: -Math.abs(parseFloat(document.getElementById('editImporto').value)),
@@ -228,11 +250,8 @@ export async function overlayEdit(spesa) {
      };
        try {
          await updateSpesa(spesa);
-         overlay.classList.remove('show');
-         mainContent.classList.remove('blur-active');
-         setTimeout(() => overlay.style.display = 'none', 300);
+         overlay.classList.remove('showOverlay');
          createCriteri();
-         form.reset();
        } catch (err) {
          showErrorToast("Errore durante la modifica:", "error");
        }
